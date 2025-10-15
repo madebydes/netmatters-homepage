b865a70a625441a6ec7288e984697c5a10e5f0b9
madebydes <madebydes@users.noreply.github.com>
2025-10-15 11:25:21 +0100

    Feat: Build responsive sidebar with push animation
    - Off-canvas sidebar with a push-content animation.
    - Hamburger button toggles the menu and animates to an 'X'.
    - Main page content is overlaid with a dark tint when the menu is active.
    - Main menu items are clickable to expand and collapse their sub-menus.
    - Includes a separate, icon-based menu layout for mobile
    
    Planning to make further improvements in sidebar

---
+++ app/js/sidebar.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  // --- BANNER SLIDER SCRIPT ---
+
+  // --- SIDEBAR SCRIPT ---
+  $(".site-nav-toggle").on("click", function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    $(this).toggleClass("is-active");
+    $("body").toggleClass("menu-is-active");
+  });
+
+  $("#container").on("click", function () {
+    if ($("body").hasClass("menu-is-active")) {
+      $("body").removeClass("menu-is-active");
+      $(".site-nav-toggle").removeClass("is-active");
+    }
+  });
+
+  // --- SIDEBAR SUB-MENU SCRIPT ---
+  $(".sidebar > ul > li > a").on("click", function (e) {
+    // Check if this menu item has a sub-menu
+    if ($(this).siblings(".sub-menu-banner").length > 0) {
+      e.preventDefault(); // Prevent link from navigating
+      $(this).parent().toggleClass("is-open");
+    }
+  });
+});
